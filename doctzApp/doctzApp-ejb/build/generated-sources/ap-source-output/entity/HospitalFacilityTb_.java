dd64ab89a074b3571faf020683672ab9358d318a
doctz <kapadianancy21@gmail.com>
2020-04-29 11:36:23 +0530

    blog1 page

--- doctzApp/doctzApp-ejb/build/generated-sources/ap-source-output/entity/HospitalFacilityTb_.java
+++ doctzApp/doctzApp-ejb/build/generated-sources/ap-source-output/entity/HospitalFacilityTb_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-28T19:58:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-29T10:47:15")
 @StaticMetamodel(HospitalFacilityTb.class)
 public class HospitalFacilityTb_ { 
 
--- doctzApp/doctzApp-ejb/build/generated-sources/ap-source-output/entity/SpecializationTb_.java
+++ doctzApp/doctzApp-ejb/build/generated-sources/ap-source-output/entity/SpecializationTb_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-28T19:58:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-29T10:47:15")
 @StaticMetamodel(SpecializationTb.class)
 public class SpecializationTb_ { 
 
