38a57702447dad9cc05d045f6f00817c3e3a2235
doctz <kapadianancy21@gmail.com>
2020-05-04 19:33:47 +0530

    test gitignore

--- doctzApp/doctzApp-ejb/build/generated-sources/ap-source-output/entity/UserTb_.java
+++ doctzApp/doctzApp-ejb/build/generated-sources/ap-source-output/entity/UserTb_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-04T19:06:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-04T19:33:04")
 @StaticMetamodel(UserTb.class)
 public class UserTb_ { 
 
