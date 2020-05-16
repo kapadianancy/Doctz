c5f84f4e1a9e5630520692c929d612485ff12846
doctz <kapadianancy21@gmail.com>
2020-05-16 13:46:04 +0530

    final app

--- doctzApp/doctzApp-ejb/build/generated-sources/ap-source-output/entity/DoctorTb_.java
+++ doctzApp/doctzApp-ejb/build/generated-sources/ap-source-output/entity/DoctorTb_.java
@@ -11,15 +11,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-16T10:27:24")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-13T11:53:11")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-13T11:56:00")
->>>>>>> 3230b5225d7bfe69c9df43b428e4154eb39360b6
->>>>>>> 91f9faa7684f7c87dd7c15d9915ffe7e002d418b
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-16T13:42:32")
 @StaticMetamodel(DoctorTb.class)
 public class DoctorTb_ { 
 
